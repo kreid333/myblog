a2cd592b619966dd76aadb92891c6c3bb57181c4
kreid333 <kai.j.reid@gmail.com>
2021-06-29 20:54:31 -0400

    added error handling for empty form submit

--- add-post.php
+++ add-post.php
@@ -44,21 +44,33 @@
 <?php include "./includes/footer.php" ?>
 <?php include "config/database.php" ?>
 
-<?php $base_url = "http://" . $_SERVER['SERVER_NAME'] . "/myblog";?>
+<?php $base_url = "http://" . $_SERVER['SERVER_NAME'] . "/myblog"; ?>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $img = $_POST["post-imgUrl"];
-    $title = $_POST["post-title"];
-    $date = $_POST["post-date"];
-    $content = $_POST["post-content"];
-    $tag = $_POST["post-tag"];
+    if (
+        $_POST["post-imgUrl"] !== "" &&
+        $_POST["post-imgUrl"] !== "" &&
+        $_POST["post-imgUrl"] !== "" &&
+        $_POST["post-imgUrl"] !== "" &&
+        $_POST["post-imgUrl"] !== ""
+    ) {
+        $img = $_POST["post-imgUrl"];
+        $title = $_POST["post-title"];
+        $date = $_POST["post-date"];
+        $content = $_POST["post-content"];
+        $tag = $_POST["post-tag"];
 
-    $sql = "INSERT INTO posts(img, title, date, content, tag) VALUES(:img, :title, :date, :content, :tag);";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(["img" => $img, "title" => $title, "date" => $date, "content" => $content, "tag" => $tag]);
+        $sql = "INSERT INTO posts(img, title, date, content, tag) VALUES(:img, :title, :date, :content, :tag);";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(["img" => $img, "title" => $title, "date" => $date, "content" => $content, "tag" => $tag]);
 
-    header("Location: $base_url/index.php");
-    exit();
+        header("Location: $base_url/index.php");
+        exit();
+    } else {
+        echo "<script>
+        alert('Please enter an image url!');
+        </script>";
+    }
 }
 ?>
