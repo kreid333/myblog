346a3101c3b504e21e89b6f780ac052b0a7b47f0
kreid333 <kai.j.reid@gmail.com>
2021-07-08 13:43:25 -0400

    edited database connection to include JAWSDB for heroku deployment

--- config/database.php
+++ config/database.php
@@ -1,15 +1,33 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dsn = "mysql:host=$servername;dbname=myblog";
+
+$servername;
+$username;
+$password;
+$database;
+$dsn;
+
+if (getenv('JAWSDB_URL')) {
+  $url = getenv('JAWSDB_URL');
+  $dbparts = parse_url($url);
+
+  $servername = $dbparts['host'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'], '/');
+  $dsn = "mysql:host=$servername;dbname=$database";
+} else {
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "myblog";
+  $dsn = "mysql:host=$servername;dbname=$database";
+}
 
 try {
   $conn = new PDO($dsn, $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $conn;
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
