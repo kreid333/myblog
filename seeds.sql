c741aef76c7ec16bf26dcfb15cf2ea511f3e855c
kreid333 <kai.j.reid@gmail.com>
2021-07-08 13:47:45 -0400

    added brackets to composer.json and rearranged order for seeds.sql statements

--- seeds.sql
+++ seeds.sql
@@ -18,11 +18,11 @@ INSERT INTO posts(img, date, title, content, tag) VALUES(
 );
 
 INSERT INTO posts(img, date, title, content, tag) VALUES(
-    "https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://images.pexels.com/photos/1030928/pexels-photo-1030928.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     "2021-07-31",
-    "My experience in New York",
+    "Best cameras of 2021",
     "I'm baby viral hoodie gentrify food truck, tumeric lomo crucifix succulents banjo hexagon organic wolf 3 wolf moon truffaut. Adaptogen bespoke helvetica disrupt kinfolk tumeric hella cardigan 3 wolf moon +1 try-hard pok pok. Put a bird on it heirloom godard, coloring book vinyl slow-carb aesthetic vape cliche ethical 3 wolf moon kale chips fixie woke cornhole. Yuccie vegan man bun gluten-free single-origin coffee, raw denim ennui prism umami before they sold out humblebrag williamsburg shoreditch. Vegan selfies fashion axe pok pok tbh viral, unicorn narwhal aesthetic hammock. Kogi scenester quinoa taiyaki green juice next level cray tousled tbh yuccie keytar pug.",
-    "TRAVEL"
+    "PHOTOGRAPHY"
 );
 
 INSERT INTO posts(img, date, title, content, tag) VALUES(
@@ -34,9 +34,9 @@ INSERT INTO posts(img, date, title, content, tag) VALUES(
 );
 
 INSERT INTO posts(img, date, title, content, tag) VALUES(
-    "https://images.pexels.com/photos/1030928/pexels-photo-1030928.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     "2021-07-31",
-    "Best cameras of 2021",
+    "My experience in New York",
     "I'm baby viral hoodie gentrify food truck, tumeric lomo crucifix succulents banjo hexagon organic wolf 3 wolf moon truffaut. Adaptogen bespoke helvetica disrupt kinfolk tumeric hella cardigan 3 wolf moon +1 try-hard pok pok. Put a bird on it heirloom godard, coloring book vinyl slow-carb aesthetic vape cliche ethical 3 wolf moon kale chips fixie woke cornhole. Yuccie vegan man bun gluten-free single-origin coffee, raw denim ennui prism umami before they sold out humblebrag williamsburg shoreditch. Vegan selfies fashion axe pok pok tbh viral, unicorn narwhal aesthetic hammock. Kogi scenester quinoa taiyaki green juice next level cray tousled tbh yuccie keytar pug.",
-    "PHOTOGRAPHY"
+    "TRAVEL"
 );
