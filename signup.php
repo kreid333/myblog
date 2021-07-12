b9801a71843f354c5a85a1619ffccf88a486008f
kreid333 <kai.j.reid@gmail.com>
2021-07-12 14:03:25 -0400

    added brackets around variable in header function to test on heroku

--- signup.php
+++ signup.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->execute(["user_id" => $uuid, "email" => $email, "username" => $username, "password" => $hashedPassword]);
             $_SESSION["id"] = $uuid;
             $_SESSION["username"] = $username;
-            header("Location: $base_url/add-post.php");
+            header("Location: {$base_url}/add-post.php");
             exit();
         } catch (PDOException $e) {
             echo "<script>
